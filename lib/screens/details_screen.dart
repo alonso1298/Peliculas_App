49bc2470d54121eae54bcb224caef0da40030f9f
Alonso Sagrero <55163474+alonso1298@users.noreply.github.com>
2023-11-09 18:17:24 -0600

    Se agregan 2 pantallas y el archivo de exportacion

---
+++ lib/screens/details_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Center(
+          child: Text('Home Screen'),
+        ),
+      )
+    );
+  }
+}
